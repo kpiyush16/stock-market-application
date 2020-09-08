94339c1e22d86afab57bbd964a01452babe94799
pockemon <hardikrana276@gmail.com>
2020-09-08 22:46:37 +0530

    ImportExcel server side working

--- ImportExcel/src/main/java/com/example/ImportExcel/entity/ImportExcelEntity.java
+++ ImportExcel/src/main/java/com/example/ImportExcel/entity/ImportExcelEntity.java
@@ -1,6 +1,8 @@
 package com.example.ImportExcel.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.sql.Time;
 import java.util.Date;
@@ -9,8 +11,11 @@ import java.util.Date;
 public class ImportExcelEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+
     private String Company_Code;
-    private String Stock_Exchange;
+    private String stock_Exchange;
     private String Price_Per_Share;
     private String date;
     private String time;
@@ -19,12 +24,21 @@ public class ImportExcelEntity {
 
     }
 
-    public ImportExcelEntity(String company_Code, String stock_Exchange, String price_Per_Share, String date, String time) {
+    public ImportExcelEntity(int id, String company_Code, String stock_exchange, String price_Per_Share, String date, String time) {
+        Id = id;
         Company_Code = company_Code;
-        Stock_Exchange = stock_Exchange;
+        stock_Exchange = stock_exchange;
         Price_Per_Share = price_Per_Share;
         this.date = date;
         this.time = time;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
     }
 
     public String getCompany_Code() {
@@ -36,11 +50,11 @@ public class ImportExcelEntity {
     }
 
     public String getStock_Exchange() {
-        return Stock_Exchange;
+        return stock_Exchange;
     }
 
-    public void setStock_Exchange(String stock_Exchange) {
-        Stock_Exchange = stock_Exchange;
+    public void setStock_Exchange(String stock_exchange) {
+        stock_Exchange = stock_exchange;
     }
 
     public String getPrice_Per_Share() {
