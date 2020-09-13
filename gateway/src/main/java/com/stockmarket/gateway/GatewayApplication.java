fb5376b377f6766a4d693d20e418a7e91cf86ea5
pockemon <hardikrana276@gmail.com>
2020-09-13 17:32:19 +0530

    docker related files

--- gateway/src/main/java/com/stockmarket/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/stockmarket/gateway/GatewayApplication.java
@@ -2,6 +2,7 @@ package com.stockmarket.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableZuulProxy
 @CrossOrigin
+@EnableDiscoveryClient
 public class GatewayApplication {
 	
 	public static void main(String[] args) {
--- stock-exchange/src/main/java/com/stockmarket/stockexchange/StockExchangeApplication.java
+++ stock-exchange/src/main/java/com/stockmarket/stockexchange/StockExchangeApplication.java
@@ -2,6 +2,7 @@ package com.stockmarket.stockexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class StockExchangeApplication {
 
 	@LoadBalanced
